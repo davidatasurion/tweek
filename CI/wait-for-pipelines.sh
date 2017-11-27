890abd6ccb51e35afc6df88ed23608a1cae9fa62
Nataly Shrits <nataly87s@gmail.com>
2017-11-27 11:19:52 +0200

    fix wait script

--- CI/wait-for-pipelines.sh
+++ CI/wait-for-pipelines.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
 GIT_SHA=$1
-EXPECTED_IMAGES_AMOUNT=$2
-SLEEP_INTERVAL=${3-5}
-MAX_RETRIES=${4-100}
+SLEEP_INTERVAL=${2-5}
+MAX_RETRIES=${3-100}
 
 for i in $(seq $MAX_RETRIES)
 do
   echo Performing try [ $i/$MAX_RETRIES ] to get all ready images that matches git sha: $GIT_SHA
-  RESULT=$(cf-cli images ls --list -s $GIT_SHA | wc -l)
-  if [ "$RESULT" -ge "$EXPECTED_IMAGES_AMOUNT" ];
+
+  cf-cli images ls --list -s $GIT_SHA > images.txt
+
+  if [ $(grep -c "soluto/tweek-api " images.txt) -gt 0 ] && [ $(grep -c "soluto/tweek-management " images.txt) -gt 0 ] && [ $(grep -c "soluto/tweek-authoring " images.txt) -gt 0 ] && [ $(grep -c "soluto/tweek-editor " images.txt) -gt 0 ]
   then
-    echo Total ready images: [ $RESULT/$EXPECTED_IMAGES_AMOUNT ]
     echo Success: All images are ready
     echo ""
     exit 0
-  else
-    echo Total ready images: [ $RESULT/$EXPECTED_IMAGES_AMOUNT ]
+  else  
     echo "Waiting $SLEEP_INTERVAL seconds before retrying again"
     echo ""
     sleep $SLEEP_INTERVAL
